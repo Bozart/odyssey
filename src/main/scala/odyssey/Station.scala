57773e47c4045c614f7fa9d144b9ab715c9454df
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2013-05-25 17:53:06 -0400

    finish parse of the entire record set
    1.0 version is complete I guess?

--- src/main/scala/odyssey/Station.scala
+++ src/main/scala/odyssey/Station.scala
@@ -3,28 +3,106 @@ package odyssey
 import com.github.nscala_time.time.Imports._
 import scala.util.parsing.combinator.RegexParsers
 
-
 // parser to read weather station records in the WBAN inventory
 class WBANStationParser extends RegexParsers {
+  // formats are listed in:
+  // ftp://ftp.ncdc.noaa.gov/pub/data/inventories/WBAN-FMT.TXT
+  // although I don't think that file is completely right
+
+  //because data is encoded with position, we can't skip the whitespace.
   override def skipWhitespace = false
-  def Lat = Dir ~ LatDeg ~ (" " ~> Min) ~ (" " ~> Sec) ^^ {
-    case (neg~d~m~s) => Station.DMS(neg * d,m,s)
+
+  // helper functions to define regular width text or numbers
+  def txt(n: Int) = ("." * n).r ^^ {_.trim}
+  def num(n: Int) = txt(n) ^^ { _.toInt }
+  def maybetxt(n: Int) = txt(n) ^^ {
+    _ match {
+      case "" => None
+      case x  => Some(x)
+    }
   }
-  def Lon = Dir ~ LonDeg ~ (" " ~> Min) ~ (" " ~> Sec) ^^ {
-    case (neg~d~m~s) => Station.DMS(neg * d,m,s)
+  def maybenum(n: Int) = txt(n) ^^ {
+    _ match {
+      case "" => None
+      case x  => Some(x.toInt)
+    }
+  }
+  // function where if we have the missing value then we return nothing,
+  // otherwise Option(v)
+  def NumMissing(v: Int, missval: Int) = v match {
+    case `missval` => None
+    case x         => Some(x)
+  }
+  def DateMissing(d: LocalDate, missval: LocalDate) = d match {
+    case `missval` => None
+    case x         => Some(x)
   }
   
-  def Dir: Parser[Int] = """ |-""".r ^^ {
-    case " " => 1  // indicates a northern latitude or eastern longitude
-    case "-" => -1 // indicates a southern latitude or western longitude 
+  // read a date in yyyymmdd format
+  def yyyymmdd = num(4) ~ num(2) ~ num(2) ^^ {case (y~m~d) => new LocalDate(y,m,d)}
+  val unknownStartDate = new LocalDate(0,1,1)
+  val unknownEndDate = new LocalDate(9999,12,31)
+  
+  def startdate = yyyymmdd ^^ {DateMissing(_,unknownStartDate)}
+  def enddate = yyyymmdd ^^ {DateMissing(_,unknownEndDate)}
+  def DateSpan = startdate ~ (" " ~> enddate) ^^ {
+    case (start~end) => Station.DateSpan(start, end)
   }
   
-  def LatDeg: Parser[Int] = """\d{2}""".r ^^ { _.toInt }
-  def LonDeg: Parser[Int] = """\d{3}""".r ^^ { _.toInt }
-  def Min: Parser[Int] = """\d{2}""".r ^^ { _.toInt }
-  def Sec: Parser[Int] = """\d{2}""".r ^^ { _.toInt }
+  // parser indicating that we have up to n numbers, and that if the 
+  // number matches the missing value, return None.
+  def optnum(n: Int, missval: Int) = num(n) ^^ {NumMissing(_, missval)}
+  
+  def LatDeg = num(3)
+  def LonDeg = num(4)
+  def Min = num(2)
+  def Sec = num(2)
+  
+  def Lat = LatDeg ~ (" " ~> Min) ~ (" " ~> Sec) ^^ {
+    case (d~m~s) => Station.DMS(d,m,s)
+  }
+  def Lon = LonDeg ~ (" " ~> Min) ~ (" " ~> Sec) ^^ {
+    case (d~m~s) => Station.DMS(d,m,s)
+  }
+  
+  def Ele = optnum(6,-99999)
+  def EleType = maybenum(2)
   
   
+  def Location = Lat ~ (" " ~> Lon) ~ (" " ~> Ele) ~ (" " ~> Ele) ~ (" " ~> EleType) ^^ {
+    case (lat~lon~eleg~ele~typ) => Station.LatLonElevation(lat,lon,eleg,ele,typ) 
+  }
+  def BlankEnd = ".*".r
+  def WBAN = 
+      maybetxt(6) ~              // 01 - 06     Coop Station Id
+      (" " ~> maybetxt(2)) ~     // 08 - 09     Climate Division
+      (" " ~> txt(5)) ~          // 11 - 15     WBAN Station Id <- this is the primary key?
+      (" " ~> maybetxt(5)) ~     // 17 - 21     WMO Station Id
+      (" " ~> maybetxt(4)) ~     // 23 - 26     FAA LOC ID
+      (" " ~> maybetxt(5)) ~     // 28 - 32     NWS Location Identifier
+      (" " ~> maybetxt(4)) ~     // 34 - 37     ICAO Station Id
+      (" " ~> maybetxt(20)) ~    // 39 - 58     Country
+      (" " ~> maybetxt(2)) ~     // 60 - 61     State/Province Abbreviation  ( United States/Canada Only )
+      (" " ~> maybetxt(30)) ~    // 63 - 92     County Name ( United States Only ) 
+      (" " ~> maybetxt(5)) ~     // 94 - 98     Time Zone ( Number of Hours Added to Local Time to Get Greenwich Time )
+      (" " ~> maybetxt(30)) ~    //100 - 129    Historical Coop Station Name
+      (" " ~> maybetxt(30)) ~    //131 - 160    Historical WBAN Station Name
+      (" " ~> DateSpan) ~        //162 - 169    Beginning Date of Period of Record ( YYYYMMDD )  ( "00000101" => Unknown Begin Date )
+                                 //171 - 178    Ending Date of Period of Record ( YYYYMMDD )  ( "99991231" => Station Currently Open )
+      (" " ~> Location) ~        //180 - 180    Latitude Direction  ( " " => North, "-" => South ) //181 - 182    Latitude Degrees //184 - 185    Latitude Minutes  //187 - 188    Latitude Seconds
+                                 //191 - 193    Longitude Direction  ( " " => East, "-" => West )  //195 - 196    Longitude Degrees  //198 - 199    Longitude Minutes //201 - 202    Longitude Seconds
+                                 // I think this is not actually in the data!!!
+                                 //201 - 202     Latitude/Longitude Precision Code ( "54" => Degrees, Whole Minutes ) ( "55" => Degrees, Whole Minutes, Whole Seconds ) ( "63" => Degrees, Decimal Minutes to Hundredths ) ( "64" => Degrees, Decimal Minutes to Thousandths ) ( "66" => Deg, Minutes, Decimal Seconds to Tenths ) ( "67" => Deg, Min, Decimal Seconds to Hundredths )
+                                 // that means you should subtract 3 from the following ones.
+                                 //204 - 209     Elevation - Ground  ( Feet )
+                                 //211 - 216     Elevation  ( Feet )
+                                 //218 - 219     Elevation Type Code ( "0" => Unknown Elevation Type ) ( "2" => Barometer Ivory Point ) ( "6" => Ground ) ( "7" => Airport )
+      (" " ~> maybetxt(11)) ~    //221 - 231     Station Relocation
+      (" " ~> maybetxt(50)) ~    //233 - 282     Station Types
+      (BlankEnd) ^^              //283 - 284     Blank
+      {case         (coopid~cd~wbanid~wmoid~faaid~nwsid~icaoid~country~state~county~tz~histcoop~histwban~datespan~loc~reloc~typ~b) =>
+        Station.WBAN(coopid,cd,wbanid,wmoid,faaid,nwsid,icaoid,country,state,county,tz,histcoop,histwban,datespan,loc,reloc,typ)
+      }
 }
 
 
@@ -33,9 +111,6 @@ object Station {
   
   // first goal is to be able to read the inventory of WBAN stations from NOAA at
   // ftp://ftp.ncdc.noaa.gov/pub/data/inventories/WBAN.TXT
-  // formats are listed in:
-  // ftp://ftp.ncdc.noaa.gov/pub/data/inventories/WBAN-FMT.TXT
-  // although I don't think that file is completely right
 
   // TODO: replace .TXT reference with 
   // ftp://ftp.ncdc.noaa.gov/pub/data/inventories/WBAN.TXT.Z
@@ -51,7 +126,6 @@ object Station {
   case class LatLonElevation(
       Latitude:          DMS,
       Longitude:         DMS,
-      LatLonPrecision:   String,
       ElevationGround:   Option[Int],
       Elevation:         Option[Int],
       ElevationTypeCode: Option[Int]
@@ -66,33 +140,24 @@ object Station {
       )
   
   // complete station data
-  case class WBAN(
-      CoopStationID:                  Option[String],  // 01 - 06     Coop Station Id
-      ClimateDivision:                Option[String],  // 08 - 09     Climate Division
-      WBANStationID:                  String,          // 11 - 15     WBAN Station Id <- this is the primary key?
-      WMOStationID:                   Option[String],  // 17 - 21     WMO Station Id
-      FAALOCID:                       Option[String],  // 23 - 26     FAA LOC ID
-      NWSLocationIdentifier:          Option[String],  // 28 - 32     NWS Location Identifier
-      ICAOStationID:                  Option[String],  // 34 - 37     ICAO Station Id
-      Country:                        Option[String],  // 39 - 58     Country
-      StateProvinceAbbreviation:      Option[String],  // 60 - 61     State/Province Abbreviation  ( United States/Canada Only )
-      CountyName:                     Option[String],  // 63 - 92     County Name ( United States Only ) 
-      TimeZone:                       Option[String],  // 94 - 98     Time Zone ( Number of Hours Added to Local Time to Get Greenwich Time )
-      HistoricalCoopStationName:      Option[String],  //100 - 129    Historical Coop Station Name
-      HistoricalWBANStationName:      Option[String],  //131 - 160    Historical WBAN Station Name
-      DateSpan:                       DateSpan,        //162 - 169    Beginning Date of Period of Record ( YYYYMMDD )  ( "00000101" => Unknown Begin Date )
-                                                       //171 - 178    Ending Date of Period of Record ( YYYYMMDD )  ( "99991231" => Station Currently Open )
-      Location:                       LatLonElevation  //180 - 180    Latitude Direction  ( " " => North, "-" => South ) //181 - 182    Latitude Degrees //184 - 185    Latitude Minutes  //187 - 188    Latitude Seconds
-                                                       //191 - 193    Longitude Direction  ( " " => East, "-" => West )  //195 - 196    Longitude Degrees  //198 - 199    Longitude Minutes //201 - 202    Longitude Seconds
-                                                       // I think this is not actually in the data!!!
-                                                       //201 - 202     Latitude/Longitude Precision Code ( "54" => Degrees, Whole Minutes ) ( "55" => Degrees, Whole Minutes, Whole Seconds ) ( "63" => Degrees, Decimal Minutes to Hundredths ) ( "64" => Degrees, Decimal Minutes to Thousandths ) ( "66" => Deg, Minutes, Decimal Seconds to Tenths ) ( "67" => Deg, Min, Decimal Seconds to Hundredths )
-                                                       // that means you should subtract 3 from the following ones.
-                                                       //204 - 209     Elevation - Ground  ( Feet )
-                                                       //211 - 216     Elevation  ( Feet )
-                                                       //218 - 219     Elevation Type Code ( "0" => Unknown Elevation Type ) ( "2" => Barometer Ivory Point ) ( "6" => Ground ) ( "7" => Airport )
-                                                       //221 - 231     Station Relocation
-                                                       //233 - 282     Station Types
-                                                       //283 - 284     Blank
+  case class WBAN(  // note: WBANStationID and DateSpan constitute a candidate key
+      CoopStationID:                  Option[String],  
+      ClimateDivision:                Option[String],  
+      WBANStationID:                  String,          
+      WMOStationID:                   Option[String],  
+      FAALOCID:                       Option[String],  
+      NWSLocationIdentifier:          Option[String],  
+      ICAOStationID:                  Option[String],  
+      Country:                        Option[String],  
+      StateProvinceAbbreviation:      Option[String],  
+      CountyName:                     Option[String],   
+      TimeZone:                       Option[String],  
+      HistoricalCoopStationName:      Option[String],  
+      HistoricalWBANStationName:      Option[String],  
+      DateSpan:                       DateSpan,        
+      Location:                       LatLonElevation, 
+      Relocation:                     Option[String],
+      StationTypes:                   Option[String] 
   )
   
   lazy val WBANInventoryText = io.Source.fromURL("ftp://ftp.ncdc.noaa.gov/pub/data/inventories/WBAN.TXT")
@@ -106,45 +171,5 @@ object Station {
 //305803 04 94789                       UNITED STATES        NY QUEENS                         +5    NEW YORK IDLEWILD AP           NEW YORK IDLEWILD AP           19501231 19510101  40 39 00 -073 47 00     20     20  6             AIRWAYS COOP UPPERAIR                                 20  
 //          04776                       CANADA               QC                                      SENNETERRE                     SENNETERRE                     19600401 19601231  48 20 00 -077 11 00 -99999   1113  0                                                               -99999  
 //          33034       DABB            ALGERIA                                                      ANNABA                         ANNABA                         19450601 19451031  36 50 00  007 48 00 -99999      4  0                                                               -99999  
-  
-  def parseLine(L: String): List[String] = {
-    val LineMatch = ("^(.{6})." +  // 01 - 06    Coop Station Id
-                     "(.{2})." +  // 08 - 09     Climate Division
-                     "(.{5})." +  // 11 - 15     WBAN Station Id <- this is the primary key?
-                     "(.{5})." +  // 17 - 21     WMO Station Id
-                     "(.{4})." +  // 23 - 26     FAA LOC ID
-                     "(.{5})." +  // 28 - 32     NWS Location Identifier
-                     "(.{4})." +  // 34 - 37     ICAO Station Id
-                     "(.{20})." +  // 39 - 58     Country
-                     "(.{2})." +  // 60 - 61     State/Province Abbreviation  ( United States/Canada Only )
-                     "(.{30})." +  // 63 - 92     County Name ( United States Only ) 
-                     "(.{5})." +  // 94 - 98     Time Zone ( Number of Hours Added to Local Time to Get Greenwich Time )
-                     "(.{30})." +  //100 - 129    Historical Coop Station Name
-                     "(.{30})." +  //131 - 160    Historical WBAN Station Name
-                     "(.{17})." +  //162 - 169    Beginning Date of Period of Record ( YYYYMMDD )  ( "00000101" => Unknown Begin Date )
-                                   //171 - 178    Ending Date of Period of Record ( YYYYMMDD )  ( "99991231" => Station Currently Open )
-                     "(.{37})." +  //180 - 180    Latitude Direction  ( " " => North, "-" => South ) 
-                                   //181 - 182    Latitude Degrees 
-                                   //184 - 185    Latitude Minutes  
-                                   //187 - 188    Latitude Seconds
-                                   //190 - 190    Longitude Direction  ( " " => East, "-" => West )  
-                                   //191 - 193    Longitude Degrees  <-- this is a mistake in the NOAA doc
-                                   //195 - 196    Longitude Minutes 
-                                   //198 - 199    Longitude Seconds
-                                   //201 - 206     Elevation - Ground  ( Feet )
-                                   //208 - 213     Elevation  ( Feet )
-                                   //216 - 216     Elevation Type Code ( "0" => Unknown Elevation Type ) ( "2" => Barometer Ivory Point ) ( "6" => Ground ) ( "7" => Airport )
-                     "(.{11})." + //218 - 228     Station Relocation
-                     "(.{49})." +    //230 - 278     Station Types
-                     "(.{6}).*").r //280 - 285     Don't know???  then two blank
-     val LineMatch(coop,cdiv,wban,wmo,faa,nws,icao,
-         country,state,county,tz,histcoop,histwban,
-         daterange,loc,reloc,types,dontknow) = L
-     
-     coop :: cdiv :: wban :: wmo :: faa :: nws :: icao ::
-       country :: state :: county :: tz :: histcoop ::
-       histwban :: daterange :: loc :: reloc :: types ::
-       dontknow :: Nil
-  }
 
 }
--- src/test/scala/odyssey/WeatherSuite.scala
+++ src/test/scala/odyssey/WeatherSuite.scala
@@ -2,6 +2,8 @@ package odyssey
 
 import org.scalatest._
 import Station._
+import com.github.nscala_time.time.Imports._
+
 class StationParseSpec extends FlatSpec with matchers.ShouldMatchers {
   "A positive latitude" should "be broken into a DMS case class" in {
     val posLatText = " 34 43 00"
@@ -52,7 +54,71 @@ class StationParseSpec extends FlatSpec with matchers.ShouldMatchers {
     Result should equal (DMS(-3,48,1))
 
   }
-  
-    
+  "a lat, lon, and elevation" should "be put into LatLonElevation case class" in {
+    val LocData = " 40 46 00 -073 52 00 -99999     52  2"
+    val p = new WBANStationParser()
+    val Result = p.parseAll(p.Location, LocData) match {
+      case p.Success(x, _)     => x
+      case p.Failure(msg, _)   => fail(msg)
+      case p.Error(msg,_)      => fail(msg)
+    }
 
+    Result should equal (LatLonElevation(DMS(40,46,0),DMS(-73,52,0),None,Some(52),Some(2)))
+
+  }
+  "a complete date range" should "be read into a DateSpan case class" in {
+    val DateData = "19391001 19391007"
+    val p = new WBANStationParser()
+    val Result = p.parseAll(p.DateSpan, DateData) match {
+      case p.Success(x, _)     => x
+      case p.Failure(msg, _)   => fail(msg)
+      case p.Error(msg,_)      => fail(msg)
+    }
+
+    Result should equal (DateSpan(Some(new LocalDate(1939,10,1)),Some(new LocalDate(1939,10,7))))
+
+  }
+  "a date range with unknown start" should "be read into a DateSpan case class" in {
+    val DateData = "00000101 19391007"
+    val p = new WBANStationParser()
+    val Result = p.parseAll(p.DateSpan, DateData) match {
+      case p.Success(x, _)     => x
+      case p.Failure(msg, _)   => fail(msg)
+      case p.Error(msg,_)      => fail(msg)
+    }
+
+    Result should equal (DateSpan(None,Some(new LocalDate(1939,10,7))))
+
+  }
+  "a date range with unknown end" should "be read into a DateSpan case class" in {
+    val DateData = "19391001 99991231"
+    val p = new WBANStationParser()
+    val Result = p.parseAll(p.DateSpan, DateData) match {
+      case p.Success(x, _)     => x
+      case p.Failure(msg, _)   => fail(msg)
+      case p.Error(msg,_)      => fail(msg)
+    }
+
+    Result should equal (DateSpan(Some(new LocalDate(1939,10,1)),None))
+
+  }
+  "the full laguardia record" should "parse into a WBAN record" in {
+    val FullRec = "       04 14732                       UNITED STATES        NY QUEENS                         +5    LAGUARDIA                      LAGUARDIA                      19391001 19391007  40 46 00 -073 52 00 -99999     52  2             AIRWAYS                                           -99999  "
+    val p = new WBANStationParser()
+    val Result = p.parseAll(p.WBAN, FullRec) match {
+      case p.Success(x, _)     => x
+      case p.Failure(msg, _)   => fail(msg)
+      case p.Error(msg,_)      => fail(msg)
+    }
+    
+    Result should equal (WBAN(
+        None,Some("04"),"14732",None,None,None,None,Some("UNITED STATES"),
+        Some("NY"),Some("QUEENS"),Some("+5"),Some("LAGUARDIA"),Some("LAGUARDIA"),
+        DateSpan(
+            Some(new LocalDate(1939,10,1)),
+            Some(new LocalDate(1939,10,7))),
+            LatLonElevation(DMS(40,46,0),DMS(-73,52,0),None,Some(52),Some(2)),
+            None,Some("AIRWAYS")))
+  
+  }
 }
